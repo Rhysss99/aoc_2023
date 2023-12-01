f39819f1f8ad0cc4e9513e02135484b857153512
RHYS.CHORLTON <Rhys.Chorlton@fdmgroup.com>
2023-12-01 11:14:26 +0000

    day_1_part_1

--- src/main/java/org/fun/Main.java
+++ src/main/java/org/fun/Main.java
@@ -1,19 +1,48 @@
 package org.fun;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] args) throws FileNotFoundException {
+        String filePath = "input/day_1.txt"; // Path to the input file
+        int sum = 0;
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                sum += getCalibrationValue(line);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
+
+        System.out.println("Sum of calibration values: " + sum);
+    }
+
+    private static int getCalibrationValue(String line) {
+       int firstDigit = -1;
+       int lastDigit =-1;
+
+       //Find firstDigit
+        for (int i = 0; i<line.length();i++) {
+            if(Character.isDigit(line.charAt(i))) {
+                firstDigit = Character.getNumericValue(line.charAt(i));
+                break;
+            }
+        }
+        for (int i = line.length()-1; i>=0;i--) {
+            if(Character.isDigit(line.charAt(i))) {
+                lastDigit = Character.getNumericValue(line.charAt(i));
+                break;
+            }
+        }
+        if (firstDigit != -1 && lastDigit != -1) {
+            return firstDigit * 10 + lastDigit;
+        }
+        return 0;
+
     }
 }
